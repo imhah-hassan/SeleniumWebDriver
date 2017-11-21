46541c9fb7b98f2b4945d8f55931c80134559abd
hih-acial <hih@acial.fr>
2017-11-21 15:48:08 +0100

    Suite de tests

---
+++ Bpi/NonReg.java
@@ -0,0 +1,11 @@
+package formation.selenium.bpi2017;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ LoginLogout.class, AjouterSalarie.class})
+public class NonReg {
+
+}
